dd0e95c8544e2f002597a236b0a69b7262bcb810
fengyf <fengyf@nationalchip.com>
2019-07-02 15:26:28 +0800

    修复初次授权无法显示信息的BUG

--- pages/myInfo/info.js
+++ pages/myInfo/info.js
@@ -5,16 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    isLogin:app.globalData.isLogin,
-    isNeedLogin:false,
-    code:'',
-    userAvatar:'',
+    isLogin: app.globalData.isLogin,
+    isNeedLogin: false,
+    code: '',
+    userAvatar: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     let _this = this
     wx.login({
       success(res) {
@@ -22,40 +22,49 @@ Page({
           //发起网络请求
           wx.request({
             url: 'http://119.3.85.70:8085/canopus/wechat/user/getOpenId/' + res.code,
-            method: 'POST', 
-            success: function (e) {
+            method: 'POST',
+            success: function(e) {
               console.log(e)
               if (e.data.data.isFirst) {
                 wx.getSetting({
                   success: res => {
+                    console.log(res.authSetting['scope.userInfo'])
                     if (res.authSetting['scope.userInfo']) {
                       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
                       wx.getUserInfo({
                         lang: "zh_CN",
                         success: res => {
                           // 可以将 res 发送给后台解码出 unionId
-                            app.globalData.userInfo = res.userInfo
-                            console.log(res)
-                            app.globalData.isLogin = true
-                            _this.setData({
-                              isLogin: true,
-                              isNeedLogin: false,
-                              userAvatar: res.userInfo.avatarUrl,
-                              userName: res.userInfo.nickName,
-                            })
+                          app.globalData.userInfo = res.userInfo
+                          console.log(res)
+                          app.globalData.isLogin = true
+                          _this.setData({
+                            isLogin: true,
+                            isNeedLogin: false,
+                            userAvatar: res.userInfo.avatarUrl,
+                            userName: res.userInfo.nickName,
+                          })
                         },
-                        fail:function(res) {
-                          console.log("can not get user info")
+                        fail: function(res) {
+                          _this.setData({
+                            isNeedLogin: true,
+                            isLogin: false,
+                          })
                           wx.showToast({
                             title: '获取用户信息失败，需要授权',
-                            icon:'none',
+                            icon: 'none',
                             duration: 2000,
                           })
                         },
-                        })
-                      }
-                    },
-                  fail: ()=>{
+                      })
+                    } else {
+                      _this.setData({
+                        isNeedLogin: true,
+                        isLogin: false,
+                      })
+                    }
+                  },
+                  fail: () => {
                     _this.setData({
                       isNeedLogin: true,
                       isLogin: false,
@@ -64,10 +73,10 @@ Page({
                 })
               }
             },
-            fail: function (res) {
+            fail: function(res) {
               wx.showToast({
                 title: '不能请求服务器',
-                icon:'none',
+                icon: 'none',
                 duration: 2000,
               })
             }
@@ -87,55 +96,55 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-   
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
   onLoginClick: function() {
-   
+
   },
-  onGotUserInfo: function (e) {
+  onGotUserInfo: function(e) {
     let _this = this;
     console.log(e.detail.errMsg)
     console.log(e.detail.userInfo)
@@ -143,6 +152,9 @@ Page({
     app.globalData.isLogin = true
     _this.setData({
       isLogin: true,
+      isNeedLogin: false,
+      userAvatar: e.detail.userInfo.avatarUrl,
+      userName: e.detail.userInfo.nickName,
     })
   },
 
